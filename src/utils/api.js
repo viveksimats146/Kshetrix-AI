26e63ff8ca2147a5343f6b7f7fdd2451265b8e56
viveksimats146 <vivekmpv1304@gmail.com>
2026-08-31 08:34:03 +0530

    Change local backend API URL configuration back to localhost for USB port forwarding

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 export const getApiBaseUrl = () => {
-  // Use local Wi-Fi IP address directly to bypass ADB reverse tunnel resets
-  return 'http://10.111.176.190:8001';
+  // Use localhost for local development over ADB port forwarding
+  return 'http://localhost:8001';
 };
